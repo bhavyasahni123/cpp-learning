abdcd61235f779c86503642a9dec6ca8570ccadc
bhavyasahni123 <bhavyasahni012@gmail.com>
2026-08-07 14:54:01 +0530

    r2 problem kattis

---
+++ r2.cpp
@@ -0,0 +1,11 @@
+// Problem-https://open.kattis.com/problems/r2 
+#include <iostream>
+using namespace std;
+int main(){
+    int S;
+    int R1;
+    int R2;
+    cin>>R1>>S;
+    R2=(2*S)-R1;
+    cout<<R2<<endl;
+}
