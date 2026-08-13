433f63075e41c3a00000f1704c6c4146fe9f6f8f
bhavyasahni123 <bhavyasahni012@gmail.com>
2026-08-13 16:03:58 +0530

    backward repetition

---
+++ backward_repetition.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int repetition;
+    cout<<"Enter the number of repetitions:" <<endl;
+    cin>> repetition;
+    for(int i= repetition-1;i>=0;i--){
+        cout<<"This is repetition "<<i<<endl;
+    }
+
+}
