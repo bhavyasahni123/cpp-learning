e58eaf529994d3c6d487866cd648b26887b742f8
bhavyasahni123 <bhavyasahni012@gmail.com>
2026-08-13 15:55:45 +0530

    for loop

---
+++ for.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int repetitions;
+    cout<<"Enter the number of repetitions: ";
+    cin>>repetitions;
+    for(int i=0;i<repetitions;i++){
+        cout<<"This is repetition "<<i<<endl;
+    }
+}
