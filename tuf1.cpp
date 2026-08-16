4227a0b4a277a54100066da888ca45c42a4d6266
bhavyasahni123 <bhavyasahni012@gmail.com>
2026-08-16 07:11:50 +0530

    tuf1

---
+++ tuf1.cpp
@@ -0,0 +1,11 @@
+/*Complete the function printNumber which takes an integer input from the user and prints it on the screen.
+
+*/
+#include <iostream>
+using namespace std;
+int main(){
+    int N;
+        cin>>N;
+        cout<<N;
+
+}
